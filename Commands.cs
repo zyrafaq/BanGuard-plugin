2e5e73d46fdc8b7fc9ad61653cd5f1034be87727
Soofa <emr3.cbc@gmail.com>
2025-04-22 11:25:46 +0300

    Add "checkconnection" command

--- Commands.cs
+++ Commands.cs
@@ -12,6 +12,12 @@ public static class Commands
             HelpText = "Generates a connection code for linking your account."
         });
 
+        TShockAPI.Commands.ChatCommands.Add(new Command("banguard.checkconnection", CheckConnectionCmd, "checkconnection")
+        {
+            AllowServer = false,
+            HelpText = "Checks if your account is linked."
+        });
+
         Command? banCmd = TShockAPI.Commands.ChatCommands.Find(c => c.Name == "ban");
 
         if (banCmd != null)
@@ -22,6 +28,12 @@ public static class Commands
 
     private static async void ConnectCmd(CommandArgs args)
     {
+        if (args.Player.GetDiscordAccount() != null)
+        {
+            args.Player.SendErrorMessage("You are already linked to an account.");
+            return;
+        }
+
         int? code = await APIService.GenerateNewConnection(args.Player.UUID, args.Player.Name);
 
         if (code != null)
@@ -35,5 +47,19 @@ public static class Commands
         {
             args.Player.SendErrorMessage("Failed to generate connection code.");
         }
+    }
+
+    private static async void CheckConnectionCmd(CommandArgs args)
+    {
+        DCAccount? acc = args.Player.GetDiscordAccount() ?? await APIService.TryGetDiscordAccount(args.Player.UUID);
+
+        if (acc == null)
+        {
+            args.Player.SendErrorMessage("You are not linked to any account.");
+            return;
+        }
+
+        args.Player.SetDiscordAccount(acc);
+        args.Player.SendSuccessMessage($"You are linked to account {acc.DiscordName}.");
     }
 }
