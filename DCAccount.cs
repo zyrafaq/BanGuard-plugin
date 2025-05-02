194e1c21f19266a8311ba7fc2ced375e1341eac2
BanGuard <contact@banguard.uk>
2025-05-02 21:18:56 +0200

    Fix discord connections

--- DCAccount.cs
+++ DCAccount.cs
@@ -21,6 +21,12 @@ public class DCAccount
         {
             string discordName = jObject["Discord username"]!.ToString();
             string discordID = jObject["Discord ID"]!.ToString();
+            bool connected = jObject["connected"]!.Value<bool>();
+
+            if (string.IsNullOrWhiteSpace(discordName) || string.IsNullOrWhiteSpace(discordID) || !connected)
+            {
+                return null;
+            }
 
             return new DCAccount(discordName, discordID);
         }
