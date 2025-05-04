e718c8725f1295fd3a9776388b53332da931f063
BanGuard <contact@banguard.uk>
2025-05-04 08:16:01 +0200

    Set EnableDiscordConnection by default to false

--- Configuration.cs
+++ Configuration.cs
@@ -8,7 +8,7 @@ public class Configuration
 {
     public static readonly string ConfigPath = Path.Combine(TShock.SavePath, "BanGuardConfig.json");
     public string APIKey = "paste_your_api_key_here";
-    public bool EnableDiscordConnection = true;
+    public bool EnableDiscordConnection = false;
     public List<string> BadBanCategories = new List<string>();
     // You can find the valid categories list at the discord support server
     public string[] ConnectedPlayerPermissions = { };
