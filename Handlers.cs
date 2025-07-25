3e205b63459748f1f1e82a7af446cccbc1e311e8
BanGuard <contact@banguard.uk>
2025-07-25 11:38:32 +0200

    Nicer ban message

--- Handlers.cs
+++ Handlers.cs
@@ -71,7 +71,7 @@ public static class Handlers
                     banList.Add(b["category"]);
                 }
             }
-            player.Disconnect($"You are banned on the BanGuard network for {string.Join(",", banList)}.");
+            player.Disconnect($"You are banned on the BanGuard network for {string.Join(", ", banList)}.");
         }
         else if (playerBan.IsProxy && BanGuard.Config.DisallowProxyIPs)
         {
