9ef4b87e198ef84633c7f0f1ed9944aada9b5024
BanGuard <contact@banguard.uk>
2025-07-25 11:08:29 +0200

    remove some comments

--- Models/PlayerBan.cs
+++ Models/PlayerBan.cs
@@ -14,33 +14,3 @@ public class PlayerBan
 
     }
 }
-
-
-// {
-//   "banned": true,
-//   "is_proxy": true,
-//   "bans": [
-//     {
-//       "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//       "server": {
-//         "id": 0,
-//         "name": "string"
-//       },
-//       "category": "string",
-//       "creation_timestamp": "2025-07-24T17:58:53.327Z"
-//     },
-//     {
-//       "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//       "server": {
-//         "id": 0,
-//         "name": "string"
-//       },
-//       "category": "string",
-//       "creation_timestamp": "2025-07-24T17:58:53.327Z"
-//     }
-//   ],
-//   "ban categories": [
-//     "string"
-//   ],
-//   "logged in as": "string"
-// }
