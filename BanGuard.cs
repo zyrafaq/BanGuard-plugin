e1c5ff1c2df4bcf564047df487e736481f71bda7
zyrafaq <krzesimir@hobler.pl>
2025-10-25 17:07:31 +0200

    update Author

--- BanGuard.cs
+++ BanGuard.cs
@@ -6,7 +6,7 @@ namespace BanGuard;
 [ApiVersion(2, 1)]
 public class BanGuard : TerrariaPlugin
 {
-    public override string Author => "BanGuard developers";
+    public override string Author => "Zyrafaq, Soofa";
     public override string Description => "BanGuard lets Terraria admins share bans and verify accounts via Discord.";
     public override string Name => "BanGuard";
     public override Version Version => new Version(0, 1, 9);
